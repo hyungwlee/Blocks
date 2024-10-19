0ede0bb60aa2ac480cd0b4d8bcce9d6adeb2de64
Prabhdeep Brar <psbrar2002@gmail.com>
2024-10-18 21:50:44 -0700

    Initial Commit

---
+++ Blocks/BlocksApp.swift
@@ -0,0 +1,17 @@
+//
+//  BlocksApp.swift
+//  Blocks
+//
+//  Created by Prabhdeep Brar on 10/18/24.
+//
+
+import SwiftUI
+
+@main
+struct BlocksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Blocks/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Blocks
+//
+//  Created by Prabhdeep Brar on 10/18/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
