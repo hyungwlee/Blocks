f2be93f35924992868524cdcee63bb8baa64c488
jevon <jevonwilliams85@yahoo.com>
2024-11-18 16:37:54 -0500

    new blocks t and z

--- Code/Nodes/Custom Nodes/BZShapeBlock.swift
+++ Code/Nodes/Custom Nodes/BZShapeBlock.swift
@@ -1,5 +1,5 @@
 //
-//  BT5Block.swift
+//  BZShapeBlock.swift
 //  Blocks
 //
 //  Created by Jevon Williams on 11/18/24.
@@ -8,18 +8,18 @@
 import Foundation
 import SpriteKit
 
-class BTShapedBlock: BBoxNode {
+class BZShapedBlock: BBoxNode {
     // List of possible asset names for the block
     private let availableAssets = [
-        "T-Block-1", "T-Block-2", "T-Block", // Example assets
-        "Group 16309-1", "Group 16309", "Group 16310", "Group 16312-1", "Group 16313", "Group 16314-1", "Group 16316", "Group 16363-1", "Dead"
+        "Z-Block-1", "Z-Block-2", "Z-Block", // Example assets
+        "Group 16309-1", "Group 16309", "Group 16310", "Group 16312-1", "Group 16313", "Group 16314-1", "Group 16316", "Group 16363-1","Group 16316-1"
     ]
     
     // Dictionary mapping asset names to colors
     private let assetColors: [String: UIColor] = [
-        "T-Block-1": .red,   // Example color for T-Block-1
-        "T-Block-2": .green, // Example color for T-Block-2
-        "T-Block": .blue,    // Example color for T-Block
+        "Z-Block-1": .red,   // Example color for Z-Block-1
+        "Z-Block-2": .green, // Example color for Z-Block-2
+        "Z-Block": .blue,    // Example color for Z-Block
         "Group 16309-1": .yellow,
         "Group 16309": .orange,
         "Group 16310": .purple,
@@ -28,27 +28,27 @@ class BTShapedBlock: BBoxNode {
         "Group 16314-1": .brown,
         "Group 16316": .blue,
         "Group 16363-1": .yellow,
-        "Dead": .green
+        "Group 16316-1": .red
     ]
     
-    // Now directly selecting a specific asset (e.g., "T-Block-1")
-    private let selectedAsset = "Dead"  // Choose the asset you want
+    // Now directly selecting a specific asset (e.g., "Z-Block-1")
+    private let selectedAsset = "Group 16316-1"  // Choose the asset you want
     
     required init(layoutInfo: BLayoutInfo, tileSize: CGFloat, color: UIColor = .blue) {
         super.init(layoutInfo: layoutInfo, tileSize: tileSize, color: color)
         
-        // Define the T-shape with relative positions
+        // Define the Z-shape with relative positions
         let shapeCells = [
-            (row: -1, col: 0),  // Top block (centered)
-            (row: 0, col: -1),  // Left block
-            (row: 0, col: 0),   // Center block
-            (row: 0, col: 1)    // Right block
+            (row: 0, col: 1),  // Top-right block
+            (row: 0, col: 0),  // Top-left block
+            (row: 1, col: 0),  // Bottom-left block
+            (row: 1, col: -1)  // Bottom-right block
         ]
         
         // Assign the selected asset and its color
         let blockColor = assetColors[selectedAsset] ?? .blue  // Default color if not found
         
-        // Define assets for each part of the T-shaped block
+        // Define assets for each part of the Z-shaped block
         let assets = shapeCells.map { position in
             (name: selectedAsset, position: position)
         }
@@ -56,7 +56,7 @@ class BTShapedBlock: BBoxNode {
         // Set the color of the block to match the selected asset
         self.color = blockColor
         
-        // Initialize the T-shape with assets
+        // Initialize the Z-shape with assets
         setupShape(shapeCells, assets: assets)
     }
 
@@ -64,5 +64,3 @@ class BTShapedBlock: BBoxNode {
         fatalError("init(coder:) has not been implemented")
     }
 }
-
-
